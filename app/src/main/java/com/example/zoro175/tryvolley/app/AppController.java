247071f13cbca0f954da97fa5314bbe4d9a057e2
zhafri shafiq <skyisthelimit175@ymail.com>
2017-03-10 22:37:30 +0800

    update class

---
+++ app/src/main/java/com/example/zoro175/tryvolley/app/AppController.java
@@ -0,0 +1,18 @@
+package com.example.zoro175.tryvolley.app;
+
+import com.example.zoro175.tryvolley.volley.LruBitmapCache;
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by zoro175 on 10/03/2017.
+ */
+
+public class AppController extends Application {
+    public static final String TAG = AppController.class.getSimpleName();
+}
